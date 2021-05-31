c0425f0ce304745b84e229419c366e389f3696cb
William Candillon <wcandillon@gmail.com>
2021-05-31 20:01:39 +0200

    :two:

--- src/Intro/Intro.tsx
+++ src/Intro/Intro.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { View } from "react-native";
-import { useCurrentFrame } from "remotion";
+import { Easing, interpolate, useCurrentFrame, useVideoConfig } from "remotion";
+
+import { Extrapolate } from "../components/Redash";
 
 import Square from "./Square";
 
 const Intro = () => {
+  const { width, height } = useVideoConfig();
+  const frame = useCurrentFrame();
+  const r = interpolate(frame, [3 * 30, 3 * 30 + 13], [0, width * Math.SQRT2], {
+    ...Extrapolate.CLAMP,
+    easing: Easing.inOut(Easing.ease),
+  });
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Square />
-    </View>
+    <svg width={width} height={height}>
+      <g mask="url(#mask)">
+        <rect width="3840" height="2160" fill="#F2F2F2" />
+        <Square />
+      </g>
+      <mask id="mask">
+        <rect x={0} y={0} width={width} height={height} fill="white" />
+        <circle r={r} cx={width / 2} cy={height / 2} fill="black" />
+      </mask>
+    </svg>
   );
 };
 
--- src/Intro/Square.tsx
+++ src/Intro/Square.tsx
@@ -1,63 +1,80 @@
-import { Easing, interpolate, useCurrentFrame } from "remotion";
+import { Easing, interpolate, useCurrentFrame, useVideoConfig } from "remotion";
 
 import { Extrapolate } from "../components/Redash";
 
 const SIZE = 1225;
+
 const Square = () => {
+  const { width, height } = useVideoConfig();
   const frame = useCurrentFrame();
-  const size = interpolate(frame, [0, 30, 60, 90], [0, SIZE, SIZE, 0], {
+  const size = interpolate(frame, [0, 30, 60, 90 + 13], [0, SIZE, SIZE, 0], {
     ...Extrapolate.CLAMP,
     easing: Easing.inOut(Easing.ease),
   });
-
   return (
-    <svg width={1525} height={1525} viewBox="0 0 1525 1525" fill="none">
-      <g filter="url(#shadow)" mask="url(#mask)">
+    <>
+      <g filter="url(#shadow)">
         <rect
-          x={150 + (SIZE - size) / 2}
-          y={150 + (SIZE - size) / 2}
+          x={width / 2 - size / 2}
+          y={height / 2 - size / 2}
           width={size}
           height={size}
-          rx={150}
+          rx="50"
           fill="#F2F2F2"
         />
-        <circle r={200} cx={1525 / 2} cy={1525 / 2} fill="black" />
       </g>
       <defs>
-        <mask id="mask">
-          <rect x={0} y={0} width={1525} height={1525} fill="white" />
-          <circle r={200} cx={1525 / 2} cy={1525 / 2} fill="black" />
-        </mask>
         <filter
           id="shadow"
-          x={0}
-          y={0}
-          width={1525}
-          height={1525}
+          x="1157"
+          y="317"
+          width="1525"
+          height="1525"
           filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
+          color-interpolation-filters="sRGB"
         >
-          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
+            type="matrix"
             values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
           />
-          <feOffset dx={-50} dy={-50} />
-          <feGaussianBlur stdDeviation={50} />
-          <feColorMatrix values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.25 0" />
-          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow" />
+          <feOffset dx="-50" dy="-50" />
+          <feGaussianBlur stdDeviation="50" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow"
+          />
           <feColorMatrix
             in="SourceAlpha"
+            type="matrix"
             values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
           />
-          <feOffset dx={50} dy={50} />
-          <feGaussianBlur stdDeviation={50} />
-          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
-          <feBlend in2="effect1_dropShadow" result="effect2_dropShadow" />
-          <feBlend in="SourceGraphic" in2="effect2_dropShadow" result="shape" />
+          <feOffset dx="50" dy="50" />
+          <feGaussianBlur stdDeviation="50" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="effect1_dropShadow"
+            result="effect2_dropShadow"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect2_dropShadow"
+            result="shape"
+          />
         </filter>
       </defs>
-    </svg>
+    </>
   );
 };
 
